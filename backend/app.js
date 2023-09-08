992c93a42f5a3d1621a180086e42b056e61a1d98
Rupantar Chowdhury <rupantarchowdhury99@gmail.com>
2023-09-08 21:17:52 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -53,13 +53,24 @@ app.use(errorHandler);
 
 //static folder
 
-app.use(express.static(path.join(__dirname, './frontend/build')));
+// app.use(express.static(path.join(__dirname, './frontend/build')));
 
-//static routes
+// //static routes
 
-app.get("*", function(req, res){
-    res.sendFile(path.join(__dirname, './frontend/build/index.html'));
-});
+// app.get("*", function(req, res){
+//     res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+// });
+
+const __dirname1 = path.resolve();
+
+
+  app.use(express.static(path.join(__dirname1, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+
+
 
 //port
 const port = process.env.PORT || 8000
